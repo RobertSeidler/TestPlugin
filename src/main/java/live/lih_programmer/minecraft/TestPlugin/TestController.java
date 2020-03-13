6c0a60b1bb4489d609fae718ecfea28f3f481e25
Robert Seidler <robert.seidler1@googlemail.com>
2020-03-13 23:08:59 +0100

    added first trys using Events and EventListener;

---
+++ src/main/java/live/lih_programmer/minecraft/TestPlugin/TestController.java
@@ -0,0 +1,30 @@
+package live.lih_programmer.minecraft.TestPlugin;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+
+class TestController implements Listener{
+    @EventHandler
+	public void onPlayerJoined(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        player.sendMessage(player.getDisplayName() + " has logged onto the Server.");
+    }
+    
+    @EventHandler
+    public void onPlayerMoved(PlayerMoveEvent event){
+        Player player = event.getPlayer(); 
+        player.sendMessage(player.getDisplayName() + " has moved.");
+    }
+
+    @EventHandler
+    public void onEntityPickupItem(EntityPickupItemEvent event){
+        Entity entity = (event.getEntity());
+        entity.sendMessage(event.getItem().getName() + " Item was picked up.");
+    }
+}
--- src/main/java/live/lih_programmer/minecraft/TestPlugin/TestPlugin.java
+++ src/main/java/live/lih_programmer/minecraft/TestPlugin/TestPlugin.java
@@ -1,12 +1,18 @@
 package live.lih_programmer.minecraft.TestPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import live.lih_programmer.minecraft.TestPlugin.TestController;
 
-public class TestPlugin extends JavaPlugin {
+public class TestPlugin extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
 		getLogger().info("onEnable has been invoked!");
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvents(new TestController(), this);
 		
 	}
 	
